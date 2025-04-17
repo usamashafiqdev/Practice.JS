587c23daebaaa7bb6fcc41766b551f3cab21725d
usamashafiqdev <itusamashafiq@gmail.com>
2025-04-17 18:49:32 +0500

    update arraymethods

--- arraysMethods.js
+++ arraysMethods.js
@@ -1,72 +1,108 @@
 
-// Arrays Push
-let fruits = ["apple", "banana"];
-fruits.push("mango");
-console.log(fruits)
+// // Arrays Push
+// let fruits = ["apple", "banana"];
+// fruits.push("mango");
+// console.log(fruits)
 
 
 
-// Pop
+// // Pop
 
-let colors = ["red", "green", "blue"];
-let lastColor = colors.pop();
-console.log(lastColor); 
-console.log(colors);    
+// let colors = ["red", "green", "blue"];
+// let lastColor = colors.pop();
+// console.log(lastColor); 
+// console.log(colors);    
 
 
-// String
-let string = [1, 2, 3];
-let results = string.toString();  
-console.log(results);  
+// // String
+// let string = [1, 2, 3];
+// let results = string.toString();  
+// console.log(results);  
 
-// Shift
-let shift = [1, 2, 3];
-let firstElement = shift.shift();  
-console.log(shift);                
-console.log(firstElement);  
+// // Shift
+// let shift = [1, 2, 3];
+// let firstElement = shift.shift();  
+// console.log(shift);                
+// console.log(firstElement);  
 
-// Unshift
-let arr = [2, 3];
-let newLength = arr.unshift(1); 
-console.log(arr);                
-console.log(newLength);   
+// // Unshift
+// let arr = [2, 3];
+// let newLength = arr.unshift(1); 
+// console.log(arr);                
+// console.log(newLength);   
 
-// Concat
-let arr1 = [1, 2];
-let arr2 = [3, 4];
-let result = arr1.concat(arr2);  
-console.log(result);
+// // Concat
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let result = arr1.concat(arr2);  
+// console.log(result);
 
-Slice
-let slice= [1, 2, 3, 4, 5];
-let total = arr.slice(1, 4);  
-console.log(total);           
-console.log(slice);      
+// Slice
+// let slice= [1, 2, 3, 4, 5];
+// let total = arr.slice(1, 4);  
+// console.log(total);           
+// console.log(slice);      
 
-// Splice
+// // Splice
 
-let splice = [1, 2, 3, 4, 5];
-let removedItems = arr.splice(2, 2, 6, 7); 
-console.log(splice);              
-console.log(removedItems); 
+// let splice = [1, 2, 3, 4, 5];
+// let removedItems = arr.splice(2, 2, 6, 7); 
+// console.log(splice);              
+// console.log(removedItems); 
 
-// ARRAY MAP METHOD
+// // ARRAY MAP METHOD
 
-let num=[23,53,51,54]
-    num.map((vlaue, index, )=>{
-        console.log(vlaue+1,index,)
-    }
+// let num=[23,53,51,54]
+//     num.map((vlaue, index, )=>{
+//         console.log(vlaue+1,index,)
+//     }
 
-)
-console.log(num)
+// )
+// console.log(num)
 
-// ARRAY Filter Method
+// // ARRAY Filter Method
 
-let num2=[22,53,51,54,10]
-let a2=num2.filter((a) =>{
-    return (a>51)
+// let num2=[22,53,51,54,10]
+// let a2=num2.filter((a) =>{
+//     return (a>51)
 
-}
+// }
 
-)
-console.log(a2)
+// )
+// console.log(a2)
+// ------------------------Length method-------------------------------------
+// let fruuits =['Apple', 'mango', 'orange','leache']
+// console.log(fruuits.length)
+
+// ------------------------Join method-------------------------------------
+
+// let fruuits =['Apple', 'mango', 'orange','leache']
+// console.log(fruuits.join(','))
+
+// ------------------------Shift method-------------------------------------
+
+// let fruuits =['Apple', 'mango', 'orange','leache']
+// // Is cheaz ka andar jo value shift hoti ha ya remove hoti ha vo store hoti ha E.g Apple
+// console.log(fruuits.shift())
+// console.log(fruuits)
+
+// ------------------------Unshift method-------------------------------------
+
+// let fruuits =['Apple', 'mango', 'orange','leache']
+// // Is cheaz ka andar jo value unshift hoti ha ya add hoti ha vo store hoti ha E.g Apple
+// console.log(fruuits.unshift('kiwi'))
+// console.log(fruuits)
+
+// ------------------------Changing Element method-------------------------------------
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(fruits[2] ='egg');
+// console.log(fruits)
+
+// ------------------------Flat method-------------------------------------
+// Convert muktidimensional array in to single array
+// const myArr = [[1,2],[3,4],[5,6]];
+// let multi =myArr.flat();
+// console.log(multi)
